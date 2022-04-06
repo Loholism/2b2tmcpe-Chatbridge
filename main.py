c398e9cf3ed409f3f8ede84f38afe5e64cc0b2e2
elpablo5542 <93844038+elpablo5542@users.noreply.github.com>
2022-04-06 22:26:32 +0200

    comment

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ async def on_connect():
 async def on_message(ctx):
   if ctx.channel.id==915274436347826227:
     if ctx.author.id==700275401314009168:
-      channel=b.get_channel()#enter the id of the channel you want to have the bot send its messages to
-      content=str(ctx.content).replace("@", "[at")
+      channel=b.get_channel() #enter the id of the channel you want to have the bot send its messages to
+      content=str(ctx.content).replace("@", "[at]")
       for i in blacklist:
         if content==f":green_circle: {i} joined the server":
           content+=" "
           break
       await channel.send(content)
     else:
-      WEBHOOK_URL="https://discord.com/api/webhooks/961344091348668496/opAAEByYqKBCtlmTdIZ6QV0GACfhJC4GO8_ssLCeNHFDADJU-iM9K_AeKhUn6a7Gji1v"
+      WEBHOOK_URL="" #enter your webhook url here
       async with ClientSession() as session:
         webhook = discord.Webhook.from_url(WEBHOOK_URL, adapter=discord.AsyncWebhookAdapter(session))
         await webhook.send(content=str(ctx.content).replace("@", "[at]"), username=ctx.author.name, avatar_url=ctx.author.avatar_url)
