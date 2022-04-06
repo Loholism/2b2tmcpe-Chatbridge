f5288bb218744f53b409b48e67aff7d1f945b187
elpablo5542 <93844038+elpablo5542@users.noreply.github.com>
2022-04-06 21:53:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import os
+from aiohttp import ClientSession
+
+b=commands.Bot(command_prefix='=', self_bot=True, help_command=None)
+
+TOKEN = os.environ.get('TOKEN')
+
+blacklist=["EDestroyer10", "Pyrobyte6948", "Bloominghat7956", "GalaxyGamer661", "KLP SUPRISE", "Chaos agent5246", "ExtinctOpti"]
+
+@b.event
+async def on_connect():
+  print("Stay halal")
+  await b.change_presence(status=discord.Status.invisible)
+
+@b.event
+async def on_message(ctx):
+  if ctx.channel.id==915274436347826227:
+    if ctx.author.id==700275401314009168:
+      channel=b.get_channel(961337454714060850)
+      content=str(ctx.content).replace("@here", "@heree").replace("@everyone", "@eveyone")
+      for i in blacklist:
+        if content==f":green_circle: {i} joined the server":
+          content+=" <@578547150875656192>"
+          break
+      await channel.send(content)
+    else:
+      WEBHOOK_URL="https://discord.com/api/webhooks/961344091348668496/opAAEByYqKBCtlmTdIZ6QV0GACfhJC4GO8_ssLCeNHFDADJU-iM9K_AeKhUn6a7Gji1v"
+      async with ClientSession() as session:
+        webhook = discord.Webhook.from_url(WEBHOOK_URL, adapter=discord.AsyncWebhookAdapter(session))
+        await webhook.send(content=str(ctx.content).replace("@here", "@heree").replace("@everyone", "@eveyone"), username=ctx.author.name, avatar_url=ctx.author.avatar_url)
+
+b.run(TOKEN)
