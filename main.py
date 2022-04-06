d057326bf3f7a1efe694bcd95357b2bd745632b1
elpablo5542 <93844038+elpablo5542@users.noreply.github.com>
2022-04-06 21:58:30 +0200

    better

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ async def on_connect():
 async def on_message(ctx):
   if ctx.channel.id==915274436347826227:
     if ctx.author.id==700275401314009168:
-      channel=b.get_channel(961337454714060850)
-      content=str(ctx.content).replace("@here", "@heree").replace("@everyone", "@eveyone")
+      channel=b.get_channel()#enter the id of the channel you want to have the bot send its messages to
+      content=str(ctx.content).replace("@", "[at")
       for i in blacklist:
         if content==f":green_circle: {i} joined the server":
           content+=" "
@@ -29,6 +29,6 @@ async def on_message(ctx):
       WEBHOOK_URL="https://discord.com/api/webhooks/961344091348668496/opAAEByYqKBCtlmTdIZ6QV0GACfhJC4GO8_ssLCeNHFDADJU-iM9K_AeKhUn6a7Gji1v"
       async with ClientSession() as session:
         webhook = discord.Webhook.from_url(WEBHOOK_URL, adapter=discord.AsyncWebhookAdapter(session))
-        await webhook.send(content=str(ctx.content).replace("@here", "@heree").replace("@everyone", "@eveyone"), username=ctx.author.name, avatar_url=ctx.author.avatar_url)
+        await webhook.send(content=str(ctx.content).replace("@", "[at]"), username=ctx.author.name, avatar_url=ctx.author.avatar_url)
 
 b.run(TOKEN)
