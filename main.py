c618628a6478673dfbf452d1c16f3d7e06d81120
El Pablo5542 <elpablo5542@gmail.com>
2023-05-06 22:49:22 +0200

    minor improvements

--- main.py
+++ main.py
@@ -2,26 +2,20 @@ import discord
 from discord.ext import commands
 from aiohttp import ClientSession
 
-b=commands.Bot(command_prefix='=', self_bot=True, help_command=None)
+bot = commands.Bot(command_prefix='=', self_bot=True, help_command=None)
 
-TOKEN = #enter your accounts token here
+token = #enter your accounts token here
+webhook_url = #enter the url of your webhook here
 
-@b.event
+@bot.event
 async def on_connect():
-  print("Stay halal")
-  await b.change_presence(status=discord.Status.invisible)
+  print("Connected as "+bot.user.name+"#"+bot.user.discriminator)
 
-@b.event
+@bot.event
 async def on_message(ctx):
   if ctx.channel.id==915274436347826227:
-    if ctx.author.id==700275401314009168:
-      channel=b.get_channel() #enter the id of the channel you want to have the bot send its messages to
-      content=str(ctx.content).replace("@", "[at]")
-      await channel.send(content)
-    else:
-      WEBHOOK_URL="" #enter your webhook url here
-      async with ClientSession() as session:
-        webhook = discord.Webhook.from_url(WEBHOOK_URL, adapter=discord.AsyncWebhookAdapter(session))
-        await webhook.send(content=str(ctx.content).replace("@", "[at]"), username=ctx.author.name, avatar_url=ctx.author.avatar_url)
+    async with ClientSession() as session:
+      webhook = discord.Webhook.from_url(webhook_url, adapter=discord.AsyncWebhookAdapter(session))
+      await webhook.send(content=str(ctx.content).replace("@", "[at]"), username=ctx.author.name, avatar_url=ctx.author.avatar_url)
 
-b.run(TOKEN)
+bot.run(token)
